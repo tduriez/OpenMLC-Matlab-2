0c8783824b49eae3e7a20cd5ed620f2264859f7e
Thomas Duriez <thomas.duriez@gmail.com>
2023-10-12 12:12:25 -0300

    Added Toy problem

--- MLC_tools/Demo/toy_problem_multi.m
+++ MLC_tools/Demo/toy_problem_multi.m
@@ -12,24 +12,30 @@ function J=toy_problem_multi(idv,params,i,fig)
 s=params.problem_variables.time;
 b=params.problem_variables.b;
 
+for i=1:length(idv)
+
 b2=b*0;
 try
 m=idv.formal;
 m=strrep(m,'S0','s');
 eval(['b2=b2+' m ';'])
-J(1,1)=sqrt(sum((b2-b).^2))/length(s);
-J(2,1)=idv.complexity;
+J(1,i)=sqrt(sum((b2-b).^2))/length(s);
+J(2,i)=idv.complexity;
 catch err
-    J(1:2,1)=params.badvalue;
+    J(1:2,i)=params.badvalue;
     fprintf(err.message);
     keyboard
+end
+c2(i,:)=b2;
+
+
 end
 
 if nargin==4
     subplot(2,1,1)
-    plot(s,b-mean(b),'*','marker','o','markersize',8,'color','k');hold on
-    plot(s,b2-mean(b2),'-k','linewidth',1.2);hold  off
-    set(gca,'fontsize',13,'xlim',[min(s(:)),max(s(:))],'ylim',[min(b(:))-0.1*(max(b(:))-min(b(:))),max(b(:))+0.1*(max(b(:))-min(b(:)))]-mean(b))
+    plot(s,b,'*','marker','o','markersize',8,'color','k');hold on
+    plot(s,c2,'-k','linewidth',1.2);hold  off
+    set(gca,'fontsize',13,'xlim',[min(s(:)),max(s(:))],'ylim',[min(b(:))-0.1*(max(b(:))),max(b(:))+0.1*(max(b(:)))]-mean(b))
     l=legend('$b_i$','${K(s_i)}$');
     set(l,'location','northwest','interpreter','latex')
     grid on
